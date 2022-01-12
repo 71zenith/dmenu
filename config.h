e0bf7998e33bb407107b63eae939f44c75b51b8b
zenith71 <shawarkoprovo2007@gmail.com>
2022-01-12 21:43:00 +0530

    Replaced noto fonts emoji with joypixels

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't us
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Jetbrains Mono:size=13",
-	"Noto Color Emoji:pixelsize=14:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=14:antialias=true:autohint=true",
 	"Symbols Nerd Font:pixelsize=20:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
